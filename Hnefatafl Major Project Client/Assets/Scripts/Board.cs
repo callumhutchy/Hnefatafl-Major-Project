efc1175d329f8c95d75a632471004135900557d4
callumhutchy <callumhutchie@hotmail.co.uk>
2018-02-02 15:19:59 +0000

    Created basic board generation with materials depending on which pieces is being created. Corners are different otherwise a checkered cream and brown pattern is used. Materials have been created for each pieces (King, Knights, Barbarians)

---
+++ Hnefatafl Major Project Client/Assets/Scripts/Board.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    int width, height;
+
+    Material creamMat;
+    Material brownMat;
+
+    Material cornerMat;
+
+    Material throneMat;
+    public List<List<GameObject>> boardTiles = new List<List<GameObject>>();
+
+    GameObject tilePrefab;
+
+    //Initialise the board
+    public void Generate(int size)
+    {
+        if (size % 2 != 0)
+        {
+            width = size;
+            height = size;
+        }
+        else
+        {
+            Debug.Log("Error that size is not an odd number");
+        }
+
+        creamMat = (Material)Resources.Load("Materials/CreamMat");
+        brownMat = (Material)Resources.Load("Materials/BrownMat");
+        cornerMat = (Material)Resources.Load("Materials/CornerMat");
+        throneMat = (Material)Resources.Load("Materials/ThroneMat");
+
+        tilePrefab = (GameObject)Resources.Load("Prefabs/BoardTile");
+
+        GenerateBoard();
+    }
+
+    public void GenerateBoard()
+    {
+        bool isBrown = true;
+        for (int i = 0; i < width; i++)
+        {
+
+            List<GameObject> row = new List<GameObject>();
+
+            for (int j = 0; j < height; j++)
+            {
+                GameObject go = GameObject.Instantiate(tilePrefab);
+                if (isBrown)
+                {
+                    go.GetComponent<Renderer>().material = brownMat;
+
+                }else
+                {
+                    go.GetComponent<Renderer>().material = creamMat;
+                }
+
+               if (isCorner(i, j))
+                {
+                    go.GetComponent<Renderer>().material = cornerMat;
+                }
+                else if (i == width / 2 && j == width / 2)
+                {
+					go.GetComponent<Renderer>().material = throneMat;
+                }
+                
+                isBrown = !isBrown;
+
+                go.transform.position = new Vector3(i, 0, j);
+                go.name = "Tile x=" + i + " z=" + j;
+                row.Add(go);
+
+                go.SetActive(true);
+
+
+            }
+        }
+    }
+
+    private bool isCorner(int x, int y)
+    {
+        if (x == 0 && y == 0)
+        {
+            return true;
+        }
+        else if (x == 0 && y == width - 1)
+        {
+            return true;
+        }
+        else if (y == 0 && x == width - 1)
+        {
+            return true;
+        }
+        else if (x == width - 1 && y == width - 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ Hnefatafl Major Project Client/Assets/Scripts/Game.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	public Board board;
+
+	public int size = 11;
+
+	// Use this for initialization
+	void Start () {
+		board.Generate(size);
+		SetupPieces();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SetupPieces(){
+		Material kingMat = (Material) Resources.Load("Materials/KingMat");
+		Material knightMat = (Material) Resources.Load("Materials/KnightMat");
+		Material BarbarianMat = (Material) Resources.Load("Materials/BarbarianMat");
+
+		GameObject piece = (GameObject) Resources.Load("Prefabs/GamePiece");
+
+		GameObject king = GameObject.Instantiate(piece);
+		king.GetComponent<Renderer>().material = kingMat;
+		king.name = "King";
+		king.transform.position = new Vector3(size / 2, 0.5f, size / 2);
+		king.SetActive(true);
+
+	}
+
+
+}
