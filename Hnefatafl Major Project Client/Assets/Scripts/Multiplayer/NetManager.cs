d3f987ac99a4f111acdc7ee5a01e1b73fa4bd5eb
callumhutchy <callumhutchie@hotmail.co.uk>
2018-04-23 13:14:28 +0100

    Revert "Merge branch 'master' of https://github.com/callumhutchy/Hnefatafl-Major-Project"
    This reverts commit e032f05af6032ddec8f764be1433f9ec8d5260bf, reversing
    changes made to c8de6eb06cf458b45f3c73cf662ed3b242dd9b22.

--- Hnefatafl Major Project Client/Assets/Scripts/Multiplayer/NetManager.cs
+++ Hnefatafl Major Project Client/Assets/Scripts/Multiplayer/NetManager.cs
@@ -89,7 +89,7 @@ public class NetManager : MonoBehaviour
             try{
 
                 IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
-                // IPEndPoint endPoint = new IPEndPoint(Dns.GetHostEntry("ec2-35-178-83-188.eu-west-2.compute.amazonaws.com").AddressList[0], port);
+                // IPEndPoint endPoint = new IPEndPoint(Dns.GetHostEntry("ec2-52-56-171-201.eu-west-2.compute.amazonaws.com").AddressList[0], port);
             Debug.Log("Attempting to connect");
             //Begin connection
             clientSocket.BeginConnect(endPoint, ConnectCallback, null);
